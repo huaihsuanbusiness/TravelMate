471beb6d8d24e8ad9c8ecdd0dcd222f9949e8555
Unknown <ilj9549@gmail.com>
2018-08-22 02:08:04 +0800

    8.21.24

--- app/src/main/java/com/huaihsuanhuang/TravelMate/model/Purchased_model.java
+++ app/src/main/java/com/huaihsuanhuang/TravelMate/model/Purchased_model.java
@@ -1,4 +1,38 @@
 package com.huaihsuanhuang.TravelMate.model;
 
 public class Purchased_model {
+    public String purchased_product_name;
+    public String purchased_product_price;
+    public String purchased_product_quantity;
+
+
+    public Purchased_model(String purchased_product_name, String purchased_product_price, String purchased_product_quantity) {
+        this.purchased_product_name = purchased_product_name;
+        this.purchased_product_price = purchased_product_price;
+        this.purchased_product_quantity = purchased_product_quantity;
+    }
+
+    public String getPurchased_product_name() {
+        return purchased_product_name;
+    }
+
+    public void setPurchased_product_name(String purchased_product_name) {
+        this.purchased_product_name = purchased_product_name;
+    }
+
+    public String getPurchased_product_price() {
+        return purchased_product_price;
+    }
+
+    public void setPurchased_product_price(String purchased_product_price) {
+        this.purchased_product_price = purchased_product_price;
+    }
+
+    public String getPurchased_product_quantity() {
+        return purchased_product_quantity;
+    }
+
+    public void setPurchased_product_quantity(String purchased_product_quantity) {
+        this.purchased_product_quantity = purchased_product_quantity;
+    }
 }

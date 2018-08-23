107318cbb60834a0d411e127da079a4e0b128cc3
JackChen <king20boy@gmail.com>
2018-08-23 20:35:45 +0800

    8/23

---
+++ app/src/main/java/com/huaihsuanhuang/TravelMate/MyApplication.java
@@ -0,0 +1,14 @@
+package com.huaihsuanhuang.TravelMate;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
--- app/src/main/java/com/huaihsuanhuang/TravelMate/Purchase/Purchased.java
+++ app/src/main/java/com/huaihsuanhuang/TravelMate/Purchase/Purchased.java
@@ -3,15 +3,16 @@ package com.huaihsuanhuang.TravelMate.Purchase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.huaihsuanhuang.TravelMate.Adapter.PurchasedAdapter;
 import com.huaihsuanhuang.TravelMate.R;
@@ -25,7 +26,7 @@ import java.util.Objects;
 public class Purchased extends AppCompatActivity {
 
     private DatabaseReference rootRef;
-    private DatabaseReference databaserequest;
+    private Query databaserequest;
     private RecyclerView purchased_recyclerview;
     private LinearLayoutManager linearLayoutManager;
     private PurchasedAdapter mAdapter;
@@ -43,12 +44,16 @@ public class Purchased extends AppCompatActivity {
         purchased_recyclerview.setAdapter(mAdapter);
         mAuth  = FirebaseAuth.getInstance();
         rootRef = FirebaseDatabase.getInstance().getReference();
-        databaserequest = rootRef.child("databaserequest").child(mAuth.getCurrentUser().getUid());
+
+        // sample hard code
+        databaserequest = rootRef.child("databaserequest").child("l5fDfujEHBSnAREGqpnl0sTED143").orderByChild("account").equalTo("huaihsuanhuang9549@gmail.com");
+
         final Cart cart = new Cart();
         databaserequest.addValueEventListener(new ValueEventListener() {
             //TODO 沒跑進來 recyclerview無法顯示
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("Purchased", "dataSnapshot.getChildrenCount():" + dataSnapshot.getChildrenCount());
 
                 for (String time : cart.formattedTime_array) {
 
